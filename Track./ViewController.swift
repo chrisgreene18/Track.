3c236de82925474ca7397c885d1ce47ed6742fae
Christopher Greene <greenecr@bc.edu>
2020-06-19 15:21:36 -0400

    first push. early outline of application

--- Track./ViewController.swift
+++ Track./ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var stockArray = ["Item 1", "Item 2", "Item 3"]
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.tableView.rowHeight = 60.0
+
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stockArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    
+    
+}
---
+++ Track./requirements.swift
@@ -0,0 +1,25 @@
+//
+//  requirements.swift
+//  Track.
+//
+//  Created by Christopher Greene on 6/19/20.
+//  Copyright © 2020 Christopher Greene. All rights reserved.
+//
+
+//
+
+//1. 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
